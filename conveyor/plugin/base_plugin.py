3069f1df790a54b03b6ceaf3acd9ed04bbb01f18
XOR-op <17672363+XOR-op@users.noreply.github.com>
2024-04-16 14:56:38 -0400

    plugin: google search

--- conveyor/plugin/base_plugin.py
+++ conveyor/plugin/base_plugin.py
@@ -2,7 +2,7 @@ class BasePlugin:
     def __init__(self):
         pass
 
-    def process_new_dat(self, data: str):
+    def process_new_dat(self, data):
         raise NotImplementedError
 
     def finish(self):
--- conveyor/plugin/search_plugin.py
+++ conveyor/plugin/search_plugin.py
@@ -1,5 +1,8 @@
+from typing import List
 from conveyor.plugin.base_plugin import BasePlugin
 from conveyor.utils import getLogger
+from bs4 import BeautifulSoup
+import requests, json
 
 logging = getLogger(__name__)
 
@@ -7,14 +10,59 @@ logging = getLogger(__name__)
 class SearchPlugin(BasePlugin):
     def __init__(self):
         super().__init__()
+        self.query = None
 
-    def process_new_dat(self, data: str):
+    def process_new_dat(self, data: dict):
         try:
-            # todo
             print(f"SearchPlugin WIP: {data}")
+            query = data.get("query")
+            if query is not None:
+                self.query = query
             return None
         except Exception as e:
             return e
 
     def finish(self):
-        pass
+        if self.query is None:
+            return None
+        else:
+            result = search(self.query, "en", "us")
+            print(f"SearchPlugin result: {result}")
+            return result
+
+
+# https://stackoverflow.com/questions/73149221/programmatically-searching-google-without-api-key-in-python
+def search(query: str, language: str, country: str) -> List:
+    # https://docs.python-requests.org/en/master/user/quickstart/#passing-parameters-in-urls
+    params = {
+        "q": query,
+        "hl": language,  # language
+        "gl": country,  # country of the search, UK -> United Kingdom
+        "start": 0,  # number page by default up to 0
+        # "num": 100          # parameter defines the maximum number of results to return.
+    }
+
+    # https://docs.python-requests.org/en/master/user/quickstart/#custom-headers
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+    }
+    data = []
+
+    html = requests.get(
+        "https://www.google.com/search", params=params, headers=headers, timeout=30
+    )
+    soup = BeautifulSoup(html.text, "lxml")
+
+    for result in soup.select(".tF2Cxc"):
+        title = result.select_one(".DKV0Md").text
+        try:
+            snippet = result.select_one(".lEBKkf span").text
+        except Exception as _:
+            snippet = None
+        links = result.select_one(".yuRUbf a")["href"]
+
+        data.append({"title": title, "snippet": snippet, "links": links})
+
+    if soup.select_one(".d6cvqb a[id=pnnext]"):
+        params["start"] += 10
+    return data
