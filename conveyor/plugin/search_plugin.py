00454e8e9329059ad23f93e25b2976b7d213e593
XOR-op <17672363+XOR-op@users.noreply.github.com>
2024-04-17 11:07:32 -0400

    plugin: search tuning WIP

--- conveyor/plugin/search_plugin.py
+++ conveyor/plugin/search_plugin.py
@@ -2,19 +2,25 @@ from typing import List
 from conveyor.plugin.base_plugin import BasePlugin
 from conveyor.utils import getLogger
 from bs4 import BeautifulSoup
-import requests, json
+import requests
+import time
 
 logging = getLogger(__name__)
+
+# requests.packages.urllib3.util.connection.HAS_IPV6 = False
 
 
 class SearchPlugin(BasePlugin):
     def __init__(self):
         super().__init__()
         self.query = None
+        self.session = None
 
     def process_new_dat(self, data: dict):
+        if self.session is None:
+            self.session = requests.Session()
+            self.session.get("https://www.google.com/generate_204")
         try:
-            print(f"SearchPlugin WIP: {data}")
             query = data.get("query")
             if query is not None:
                 self.query = query
@@ -26,13 +32,19 @@ class SearchPlugin(BasePlugin):
         if self.query is None:
             return None
         else:
-            result = search(self.query, "en", "us")
-            print(f"SearchPlugin result: {result}")
+            start = time.perf_counter()
+            result = search(self.session, self.query, "en", "us")
+            end = time.perf_counter()
+            logging.info(
+                f"SearchPlugin result: {result}\n taking {end - start} seconds"
+            )
             return result
 
 
 # https://stackoverflow.com/questions/73149221/programmatically-searching-google-without-api-key-in-python
-def search(query: str, language: str, country: str) -> List:
+def search(session: requests.Session, query: str, language: str, country: str) -> List:
+    if session is None:
+        session = requests.Session()
     # https://docs.python-requests.org/en/master/user/quickstart/#passing-parameters-in-urls
     params = {
         "q": query,
@@ -47,12 +59,13 @@ def search(query: str, language: str, country: str) -> List:
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
     }
     data = []
-
-    html = requests.get(
+    logging.debug("Searching...")
+    html = session.get(
         "https://www.google.com/search", params=params, headers=headers, timeout=30
     )
+    logging.debug(f"Status code: {html.status_code}")
     soup = BeautifulSoup(html.text, "lxml")
-
+    logging.debug("Engine inited")
     for result in soup.select(".tF2Cxc"):
         title = result.select_one(".DKV0Md").text
         try:
@@ -62,7 +75,7 @@ def search(query: str, language: str, country: str) -> List:
         links = result.select_one(".yuRUbf a")["href"]
 
         data.append({"title": title, "snippet": snippet, "links": links})
-
+    logging.debug("Parsing done")
     if soup.select_one(".d6cvqb a[id=pnnext]"):
         params["start"] += 10
     return data
