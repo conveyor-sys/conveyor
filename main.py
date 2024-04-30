e88d183dd154b5c3d9c396c55742d424c0868126
XOR-op <17672363+XOR-op@users.noreply.github.com>
2024-04-30 17:36:06 -0400

    fix: schdualing eval

--- main.py
+++ main.py
@@ -199,7 +199,7 @@ def eval_scheduling():
     else:
         time_end = time.perf_counter()
         logging.info("Ongoing: " + engine.context.requests[0].decode())
-        final_tokens_len = len(engine.context.req_runtime_stats[req_id].tokens)
+        final_tokens_len = len(engine.context.requests[0].tokens)
 
     logging.info(
         f"Speed: {(final_tokens_len-init_tokens_len)/(time_end - time_start)} tokens/s"
@@ -221,5 +221,4 @@ def good_python_example():
 
 if __name__ == "__main__":
     set_hf_token()
-    eval_search()
-    # eval_scheduling()
+    eval_scheduling()
